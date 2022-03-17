b73a46c83cd5539c75e040e4ae95eed6ef26f890
Blain Crawford <blaincrawford@gmail.com>
2022-03-17 17:29:24 -0400

    got the api fetch basic working

--- src/index.js
+++ src/index.js
@@ -4,29 +4,55 @@ import { key } from './key.js';
 
 const cityInput = document.querySelector('#city-input');
 const searchButton = document.querySelector('#search-button');
+const cityDisplay = document.querySelector('#city');
 
-const searchLocationWeather = async () => {
+const clearCityInput = () => {
+  cityInput.value = '';
+}
+const searchLocationWeather = async (e) => {
   const cityBeingSearched = cityInput.value;
-  try{
-    if(zipOrCityName(cityBeingSearched)) {
+  try {
+    if (cityInput.value === '') {
+      alert('Enter city please')
+      return;
+    }
+    if (zipOrCityName(cityBeingSearched)) {
       searchZipCode(cityBeingSearched);
     } else {
       searchName(cityBeingSearched);
     }
   } catch (error) {
-    alert('can\'t Find your city Bub~')
+    alert("can't Find your city Bub~");
   }
+  clearCityInput();
 };
 
 const zipOrCityName = (searchInput) => {
   return /\d/.test(searchInput);
-}
+};
 const searchName = async (city) => {
-  //
-}
+  const cityResponse = await fetch(
+    `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${key}`,
+    {
+      mode: 'cors',
+    },
+  );
+
+  const cityInformation = await cityResponse.json();
+  cityDisplay.innerText = cityInformation[0].name;
+  
+};
 
 const searchZipCode = async (zipcode) => {
-  //
-}
+  const zipcodeResponse = await fetch(
+    `http://api.openweathermap.org/geo/1.0/zip?zip=${zipcode}&appid=${key}`,
+    {
+      mode: 'cors',
+    },
+  );
+
+  const zipcodeInformation = await zipcodeResponse.json();
+  cityDisplay.innerText =zipcodeInformation.name;
+};
 
 searchButton.addEventListener('click', searchLocationWeather, false);
