7b973a1c90edd1d8054a0bfce2ab077e2db73dad
Blain Crawford <blaincrawford@gmail.com>
2022-03-17 16:43:17 -0400

    about to get async working correctly

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import './styles.css'
 import { key } from './key.js'
+const cityInput = document.querySelector('#city-input');
+const submitButton = document.querySelector('#submmit-button');
 
+const searchLocationWeather = async () => {
+  const location = cityInput.value; 
+  console.log(location);
+  cityInput.value = ''
+};
+
+// const searchName = async (city) => {
+//   //
+// }
+
+// const searchZipCode = async (zipcode) => {
+
+// }
+
+// submitButton.addEventListener('click', searchLocationWeather, false);
