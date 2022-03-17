74fc1aaea999d84cabf6c5acb23771cf259dc974
Blain Crawford <blaincrawford@gmail.com>
2022-03-17 16:16:47 -0400

    server is working

---
+++ main.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+var h1 = document.querySelector('h1');
+h1.innerText = 'Hello, World!';
+/******/ })()
+;
+//# sourceMappingURL=main.js.map
