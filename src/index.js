9735d60b683097fd609e4627bcfa871f0e39a11c
Blain Crawford <blaincrawford@gmail.com>
2022-03-29 16:34:53 -0400

    adding in cleanup to index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import 'regenerator-runtime/runtime';
 import './styles.css';
 import { key } from './key.js';
-import{ weatherInSearchedUnits } from './WeatherApiFunctionality.js'
-import { metricsInSearchedResults, metricsInSearchedUnits } from './dayOfMetrics.js'
-
+import { weatherInSearchedUnits } from './WeatherApiFunctionality.js';
+import {
+  metricsInSearchedResults,
+  metricsInSearchedUnits,
+} from './dayOfMetrics.js';
 
 const cityInput = document.querySelector('#city-input');
 const searchButton = document.querySelector('#search-button');
@@ -14,24 +16,27 @@ const metricSelector = document.querySelector('#metric');
 const unitToggler = document.querySelector('#unit-selector');
 let unitsForSearch = 'imperial';
 
+/**
+ * choose to view weather in imperial or metric units
+ */
 const selectDisplayUnit = () => {
   if (unitsForSearch === 'imperial') {
     unitsForSearch = 'metric';
     metricSelector.classList.add('chosen-unit');
     imperialSelector.classList.remove('chosen-unit');
-    
   } else if (unitsForSearch === 'metric') {
-    unitsForSearch = 'imperial'
+    unitsForSearch = 'imperial';
     metricSelector.classList.remove('chosen-unit');
     imperialSelector.classList.add('chosen-unit');
   }
 
-  if(cityDisplay.innerText !== '') {
-    cityInput.value = cityDisplay.innerText
+  if (cityDisplay.innerText !== '') {
+    cityInput.value = cityDisplay.innerText;
     searchCityByNameOrZipcode();
   }
-}
+};
 
+// two functions for clearing search and search errors
 const clearCityInput = () => {
   cityInput.value = '';
 };
@@ -40,6 +45,9 @@ const clearSearchError = () => {
   searchError.innerText = '';
 };
 
+/**
+ * shows red text error on search form
+ */
 const throwSearchError = () => {
   if (cityInput.value === '') {
     searchError.innerText = 'Please enter city or zipcode**';
@@ -50,28 +58,45 @@ const throwSearchError = () => {
   }
 };
 
+/**
+ * Checks to see if a search is a numberic entry, zipcide, or not
+ * @param {*} searchInput
+ * @returns boolean on whether or not search is numeric
+ */
 const zipOrCityName = (searchInput) => {
   return /\d/.test(searchInput);
 };
 
+/**
+ * Searches for a city and state combination for specifc location searches
+ * @param {*} cityAndState
+ * @returns Array with latitude and longitude of search city, state combo
+ */
 const searchCityAndState = async (cityAndState) => {
   try {
-    cityAndState = cityAndState.split(', ')
+    cityAndState = cityAndState.split(', ');
     const cityName = cityAndState[0];
     const stateName = cityAndState[1];
-    const cityAndStateResponse = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName},${stateName},US&appid=${key}`, {
-      mode: 'cors'
-    })
+    const cityAndStateResponse = await fetch(
+      `http://api.openweathermap.org/geo/1.0/direct?q=${cityName},${stateName},US&appid=${key}`,
+      {
+        mode: 'cors',
+      },
+    );
     const cityAndStateInformation = await cityAndStateResponse.json();
     cityDisplay.innerText = `${cityAndStateInformation[0].name}, ${cityAndStateInformation[0].state}`;
 
     return [cityAndStateInformation[0].lat, cityAndStateInformation[0].lon];
-
   } catch (error) {
     throwSearchError();
   }
-}
+};
 
+/**
+ * Searches API with a city name input
+ * @param {*} city
+ * @returns array with lat and long of city
+ */
 const searchName = async (city) => {
   try {
     const cityResponse = await fetch(
@@ -81,7 +106,7 @@ const searchName = async (city) => {
       },
     );
     const cityInformation = await cityResponse.json();
-    
+
     if (cityInformation[0].name && cityInformation[0].country === 'US') {
       cityDisplay.innerText = `${cityInformation[0].name}, ${cityInformation[0].state}`;
     } else {
@@ -94,6 +119,11 @@ const searchName = async (city) => {
   }
 };
 
+/**
+ * tkaes in a zipcode, checks for cities, and compares location info to make sure selection is specifc
+ * @param {*} zipcode
+ * @returns array with city lat and long
+ */
 const searchZipCode = async (zipcode) => {
   try {
     const zipcodeResponse = await fetch(
@@ -133,14 +163,18 @@ const searchZipCode = async (zipcode) => {
 
     //compare against searched longitude zipcode
     const correctLongitude = longitudeArray.reduce(function (prev, curr) {
-      return Math.abs(curr - longitudeToCheck) < Math.abs(prev - longitudeToCheck)
+      return Math.abs(curr - longitudeToCheck) <
+        Math.abs(prev - longitudeToCheck)
         ? curr
         : prev;
     });
-    
+
     //Find correct state with latitude and longitude
     for (let j = 0; j < stateInformation.length; j++) {
-      if (stateInformation[j].lat === correctLatitude && stateInformation[j].lon === correctLongitude) {
+      if (
+        stateInformation[j].lat === correctLatitude &&
+        stateInformation[j].lon === correctLongitude
+      ) {
         let stateName = stateInformation[j].state;
         cityDisplay.innerText = `${zipcodeInformation.name}, ${stateName}`;
       }
@@ -152,7 +186,12 @@ const searchZipCode = async (zipcode) => {
   }
 };
 
-
+/**
+ * Takes in city input info from form and runs search specific to parameters entered
+ * @param {*} cityBeingSearched
+ * @returns resolves information to populate current day info and
+ * sends info along to metric and weather logic
+ */
 const searchCityByNameOrZipcode = (cityBeingSearched) => {
   return new Promise((resolve, reject) => {
     cityBeingSearched = cityInput.value;
@@ -162,31 +201,34 @@ const searchCityByNameOrZipcode = (cityBeingSearched) => {
 
     if (zipOrCityName(cityBeingSearched)) {
       resolve(searchZipCode(cityBeingSearched));
-    } else if(!zipOrCityName(cityBeingSearched) && cityBeingSearched.includes(', ')) {
+    } else if (
+      !zipOrCityName(cityBeingSearched) &&
+      cityBeingSearched.includes(', ')
+    ) {
       resolve(searchCityAndState(cityBeingSearched));
     } else {
       resolve(searchName(cityBeingSearched));
     }
   })
-    .then( async (response) => {
+    .then(async (response) => {
       if (response) {
         // Send API Fetch for current day weather information
-        const currentWeatherInformation = await weatherInSearchedUnits.retrieveWeatherInformationForToday(
-          response[0], 
-          response[1]
-        )
-        
+        const currentWeatherInformation =
+          await weatherInSearchedUnits.retrieveWeatherInformationForToday(
+            response[0],
+            response[1],
+          );
+
         weatherInSearchedUnits.showAreaCurrentTemp(currentWeatherInformation);
         weatherInSearchedUnits.showCurrentWeather(currentWeatherInformation);
         clearSearchError();
         clearCityInput();
         weatherInSearchedUnits.showFiveDayForecast(response[0], response[1]);
         metricsInSearchedUnits.populateDayOfMetrics(response[0], response[1]);
-      } 
+      }
     })
     .catch((error) => {
       throwSearchError();
-      console.log(error)
     });
 };
 
@@ -203,12 +245,12 @@ cityInput.addEventListener(
   },
   false,
 );
-unitToggler.addEventListener('click', selectDisplayUnit, false)
+unitToggler.addEventListener('click', selectDisplayUnit, false);
 
 //Start App with auto population of weather
 const AutoPopulatePage = (() => {
-  cityInput.value = 'Boston'
-  searchCityByNameOrZipcode()
-})()
+  cityInput.value = 'Boston';
+  searchCityByNameOrZipcode();
+})();
 
-export { throwSearchError, unitsForSearch }
+export { throwSearchError, unitsForSearch };
